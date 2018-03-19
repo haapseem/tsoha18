b02aa5134fa959253913f57eb72f1a79aec05520
Eemil Haapsaari <eemil.haapsaari@ad.helsinki.fi>
2018-03-19 07:31:48 +0200

    added main project

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SocketService } from './socket.service';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [SocketService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/socket.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
+
+@Injectable()
+export class SocketService {
+  private url = 'http://localhost:5000';
+  private socket;
+
+  constructor() {
+    this.socket = io(this.url);
+  }
+
+  public sendMessage(message) {
+    this.socket.emit('message', message);
+  }
+
+}
