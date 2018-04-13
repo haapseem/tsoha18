6f10c3e9687798b0d02a9e124d0deb590923ac30
Eemil Haapsaari <eemil.haapsaari@ad.helsinki.fi>
2018-04-13 21:40:25 +0300

    psql

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,17 @@
 #from application import app
 from flask import Flask
 
+import os
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
-from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.db"
-app.config["SQLALCHEMY_ECHO"] = True
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
